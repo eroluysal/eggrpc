bef4005982a81ecfb056669342a006869cbd7b97
Erol Uysal <eutkuuysal@gmail.com>
2020-05-10 07:34:15 +0300

    fix a few things
    Signed-off-by: Erol Uysal <eutkuuysal@gmail.com>

--- php/index.php
+++ php/index.php
@@ -5,7 +5,7 @@ require __DIR__.'/vendor/autoload.php';
 use Proto\StatRequest;
 use Proto\StatServiceClient;
 
-$cl = new StatServiceClient('localhost:4040', [
+$cl = new StatServiceClient(':4040', [
     'credentials'=> \Grpc\ChannelCredentials::createInsecure(),
 ]);
 
@@ -13,6 +13,9 @@ $req = new StatRequest();
 $resp = $cl->add($req)->wait();
 
 list($resp, $status) = $resp;
+if (null === $resp) {
+    throw new \RuntimeException('The response could not be retrieved.');
+}
 var_dump($resp->getPid());
 var_dump($resp->getStatus());
 var_dump($resp->getMessage());
